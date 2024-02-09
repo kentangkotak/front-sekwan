0760f9ff24abc5f1e421a95efa7b07165469a5f4
kentangkotak <ariezlnd69@gmail.com>
2024-02-09 09:02:30 +0700

    wew

---
+++ src/pages/v1/master/anggotadewan/AnggotaPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-page
+     class=""
+    :class="style.componentfull?'container-no-header':'container--q-header q-pa-xs'"
+  >
+    <div class="header">
+      <HeaderComp
+        ada-per-page
+        ada-refresh
+        use-full
+        :search="store.params.q"
+        :per-page="store.params.per_page"
+        @cari="store.setSearch"
+        @refresh="store.refresh"
+        @set-per-page="store.setPerPage"
+      />
+    </div>
+    <q-card
+      flat
+      no-shadow
+      square
+      class="my-flex-1 scroll"
+    >
+      <q-scroll-area
+        style="height:calc( 100% - 40px)"
+      >
+        <ListPage />
+      </q-scroll-area>
+      <div
+        v-if="Object.keys(store.meta).length"
+        class="absolute-bottom bg-primary text-white"
+      >
+        <BottomComp
+          :meta="store.meta"
+          @go-to="store.setPage"
+        />
+      </div>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from "vue";
+import { useAnggotaDewanStore } from "src/stores/master/anggotadewan"
+import { useStyledStore } from 'src/stores/app/styled'
+
+const HeaderComp = defineAsyncComponent(() => import('../comp/HeaderComp.vue'))
+const ListPage = defineAsyncComponent(() => import('../comp/ListPage.vue'))
+const BottomComp = defineAsyncComponent(() => import('../comp/BottomComp.vue'))
+
+const store = useAnggotaDewanStore()
+const style = useStyledStore()
+
+</script>
+
+
---
+++ src/stores/master/anggotadewan.js
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+import { api } from 'src/boot/axios'
+
+export const useAnggotaDewanStore = defineStore('master_anggota_dewan', {
+  state: () => ({
+    isOpen: false,
+    // loading: false,
+    // loadingTerima: false,
+    // loadingSelesai: false,
+    // loadingSimpan: false,
+    items: [],
+     meta: {},
+    params: {
+      q: '',
+      page: 1,
+      per_page: 10,
+      // kddepo: '',
+      // flag: ['1', '2'],
+      // to: dateDbFormat(new Date()),
+      // from: dateDbFormat(new Date())
+    }
+  }),
+})
