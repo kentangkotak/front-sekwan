a9494e339706fc6762922bd55e8e8dfda922f1d0
kentangkotak <ariezlnd69@gmail.com>
2024-02-09 09:02:53 +0700

    wew

--- src/stores/app/styled.js
+++ src/stores/app/styled.js
@@ -3,14 +3,14 @@ import { defineStore } from 'pinia'
 export const useStyledStore = defineStore('style_app', {
   state: () => ({
     componentfull: false,
-    // rightDrawer: false
+    rightDrawer: false
   }),
   actions: {
     setComponentFull() {
       this.componentfull = !this.componentfull
     },
-    // setRightDrawer() {
-    //   this.rightDrawer = !this.rightDrawer
-    // }
+    setRightDrawer() {
+      this.rightDrawer = !this.rightDrawer
+    }
   }
 })
